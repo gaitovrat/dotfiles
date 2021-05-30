bab2c9f3e21c6a5efb6380721d95c5e3b590b1e7
Ratmir Gaitov <gaitovrat@gmail.com>
2021-05-30 15:14:32 +0200

    Install xcode

--- Brewfile
+++ Brewfile
@@ -19,3 +19,5 @@ brew "mas"
 # Mac app store apps
 mas "Telegram", id: 747648890
 mas "Spark", id: 1176895641
+mas "XCode", id: 497799835
+mas "Vimari", id: 1480933944
--- setup.rb
+++ setup.rb
@@ -3,12 +3,20 @@
 require 'fileutils'
 
 puts 'Bundle all brew dependencies'
+puts ''
 system 'brew bundle'
 
 home = ENV['HOME'] + '/'
 dir = __dir__ + '/'
 new_line = "\n"
 
+# Xcode first launch
+puts 'Run xcode first launch'
+puts ''
+system 'sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer'
+system 'sudo xcodebuild -runFirstLaunch'
+
 # Install zsh config
-print 'Link zsh config to ', home + '.zshrc', new_line
+print 'Link zsh config to ', home + '.zshrc', new_line 
+puts ''
 FileUtils.ln_s dir + '.zshrc', home + '.zshrc', force: true
