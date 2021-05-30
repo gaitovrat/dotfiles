f2d5f1d9f978a111b9ba461d06a6ca9ae91e29fe
Ratmir Gaitov <gaitovrat@gmail.com>
2021-05-30 13:56:09 +0200

    Update setup script

--- setup.rb
+++ setup.rb
@@ -2,10 +2,13 @@
 
 require 'fileutils'
 
-system "brew bundle"
+puts 'Bundle all brew dependencies'
+system 'brew bundle'
 
 home = ENV['HOME'] + '/'
 dir = __dir__ + '/'
+new_line = "\n"
 
 # Install zsh config
+print 'Link zsh config to ', home + '.zshrc', new_line
 FileUtils.ln_s dir + '.zshrc', home + '.zshrc', force: true
